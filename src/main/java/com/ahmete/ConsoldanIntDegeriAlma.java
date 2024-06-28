d0da10f4f17a5c057b8cf54bdea02587b4cefd9d
AhmetEris-dev <ahmet2_lock@hotmail.com>
2024-06-28 13:56:45 +0300

    first commit

---
+++ src/main/java/com/ahmete/ConsoldanIntDegeriAlma.java
@@ -0,0 +1,28 @@
+package com.ahmete;
+
+import java.util.Scanner;
+
+public class ConsoldanIntDegeriAlma {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int sayi1,sayi2,sonuc;
+        System.out.println("1. sayiyi giriniz:");
+        sayi1=sc.nextInt();
+
+        System.out.println("2. sayiyi giriniz:");
+        sayi2=sc.nextInt();
+
+        sonuc=sayi1+sayi2;
+        System.out.println("Sonuc: "+sonuc);
+
+
+
+
+
+
+    }
+}
+
+
+
+
